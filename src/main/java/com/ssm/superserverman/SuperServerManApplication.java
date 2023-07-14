11576c14630675ea010f856c4963ab39a8331c10
Chris <Chris08tm@hotmail.com>
2023-07-14 16:02:46 -0500

    Resize = false

--- src/main/java/com/ssm/superserverman/SuperServerManApplication.java
+++ src/main/java/com/ssm/superserverman/SuperServerManApplication.java
@@ -14,6 +14,7 @@ public class SuperServerManApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Super Serverman");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
